1cc2080c65391f82c13cd25707246c082b0ba0f2
ken-kaneki <msherin@kse.org.ua>
2022-02-17 20:58:29 +0200

    Finished working on the project.

--- DoodleMaps/BFS.cs
+++ DoodleMaps/BFS.cs
@@ -5,36 +5,54 @@ namespace Kse.Algorithms.Samples;
 
 public class BFS
 {
+    private static int step = 0;
     static List<Point> visited = new List<Point>();
-    public static List<Point> PaintBFS(List<Point> branch, Point start, Point goal, string[,] map)
+    public static List<Point> BFSearch(List<Point> branch, Point start, Point goal, string[,] map)
     {
-        var path = branch;
-        var paths = new List<List<Point>>();
+        step += 1;
+        //Console.WriteLine("step: " + step);
         Visit(start);
         var neighbours = GetNeighbours(start.Row, start.Column, map);
             int i = 0;
             foreach (var neighbour in neighbours)
             {
+                var path = new List<Point>(branch);
                 if (!visited.Contains(neighbour))
                 { 
-                    i += 1;
+                   i += 1;
                    Visit(neighbour);
-                   Console.WriteLine(neighbour.Column + ", " + neighbour.Row);
+                   //Console.WriteLine(i + ": " + neighbour.Column + ", " + neighbour.Row);
                    path.Add(neighbour);
                    if (neighbour.Row == goal.Row && neighbour.Column == goal.Column)
                    {
                        return path;
                    }
-
-                   var result = (PaintBFS(path, neighbour, goal, map));
+                   
+                   /*Console.WriteLine("PATH Count - " + path.Count);
+                   foreach (var point in path)
+                   {
+                       Console.Write(point.Column + ":" + point.Row + ", ");
+                   }*/
+                   
+                   var result = (BFSearch(path, neighbour, goal, map));
                    if (result[result.Count - 1].Row == goal.Row && result[result.Count - 1].Column == goal.Column)
                    {
+                       /*Console.WriteLine("RESULT Count - " + result.Count);
+                       foreach (var point in result)
+                       {
+                           Console.Write(point.Column + ":" + point.Row + ", ");
+                       }*/
                        return result;
                    }
                 }
                 
             }
-
+            
+            /*Console.WriteLine("Count - " + branch.Count);
+            foreach (var point in branch)
+            {
+                Console.Write(point.Column + ":" + point.Row + ", ");
+            }*/
             return branch;
             
             /*if (paths.Count == 0)
@@ -52,7 +70,7 @@ public class BFS
 
             void Visit(Point point)
         {
-            map[point.Row, point.Column] = ".";
+            //map[point.Row, point.Column] = ".";
             visited.Add(point);
         }
     
